b8e7d877a19ae3e1f38d5f3eac9a7f4c700f79ef
Gi-ddo <60913804+Gi-ddo@users.noreply.github.com>
2021-02-20 16:10:02 +0200

    Section 2 Initial commit

---
+++ Training Facility/Training Facility/JavaScript/button.js
@@ -0,0 +1,10 @@
+﻿/*const slider = () => {
+    const button = document.querySelector('.mobile-button');
+    const nav = document.querySelector('.nav-links');
+
+    button.addEventListener('click', () => (){
+        nav.classList.toggle('nav-active');
+    });
+}
+
+slider();*/
