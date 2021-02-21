879b45c383dcb5e627b08abf9ef8814586f1465a
Gi-ddo <60913804+Gi-ddo@users.noreply.github.com>
2021-02-21 16:52:52 +0200

    Added tables

---
+++ Training Facility/Training Facility/JavaScript/modalContent.js
@@ -0,0 +1,25 @@
+﻿var modal = document.getElementById('modalRef');
+
+var modalBtn = document.getElementById('modalBtnRef');
+
+var closeBtn = document.getElementById('closeBtnRef');
+
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', windowClick);
+
+function openModal() {
+
+    modal.style.display = 'block';
+}
+
+function closeModal() {
+
+    modal.style.display = 'none';
+}
+
+function windowClick(e) {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
+}
